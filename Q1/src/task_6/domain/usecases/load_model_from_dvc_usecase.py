abf1dbebcaa05905dbf3343af655fc09bc0eacd9
chamika <chamika@softvilmedia.com>
2026-07-18 01:24:28 +0530

    model revision added

--- Q1/src/task_6/domain/usecases/load_model_from_dvc_usecase.py
+++ Q1/src/task_6/domain/usecases/load_model_from_dvc_usecase.py
@@ -7,13 +7,15 @@ class LoadModelFromDvcUseCase:
         self.logger = logger
 
     def execute(self, model_path: str):
+        self.logger.info(f"Loading model via DVC: {model_path}")
         try:
             with dvc.api.open(
                 path=model_path,
-                mode='rb'
+                mode='rb',
+                rev='xgboost.v2'
             ) as model_file:
                 model = joblib.load(model_file)
-            self.logger.info("Loaded XGBoost model via DVC")
+            self.logger.info("Loaded XGBoost model via DVC OK")
             return model
         except Exception as e:
             self.logger.error(f"Failed to load model via DVC: {str(e)}")
